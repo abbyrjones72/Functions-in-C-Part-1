7ab2a7af64085ec5a122ef403da11e0d6794605c
Abby Jones <30660461+abbyrjones72@users.noreply.github.com>
2021-06-17 12:13:17 +0000

    added explanations for each function and why we are doing what we do

--- main.c
+++ main.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 
-/* assignment must use 3 functions:
+/* 
+   assignment must use 3 functions:
    one to get the data from the user
    one to perform the exponential calculation
    one to display the result
 */
 
-/* three functions */
+/* prototype the three functions */
 int square_this(int);
 int get_data();
 void display_result();
 
 int main(void) 
 {
+  /* yes, because get_data() returns an int, we can assign that
+     to an variable of type int */
   int num = get_data();
+
+  /* same principle as before, we can assign square_this() to
+     an int */
   int squared_number = square_this(num);
+
+  /* because display_result() takes two ints, and we have those
+     defined above, we can use them as parameters */
   display_result(num, squared_number);
   return 0;
 }
@@ -24,6 +33,8 @@ int get_data()
 {
   int number;
   printf("Enter a number to square:\n");
+
+  /* look up scanf() using man 3 scanf
   scanf("%d", &number);
   return number;
 }
@@ -31,11 +42,16 @@ int get_data()
 /* function to perform exponential calculation */
 int square_this(int number)
 {
+  /* simply returning number * number prevents any unneccesary
+     memory allocations using variables */
   return number * number;
 }
 
 /* function to display the result */
 void display_result(int number, int func)
 {
+  /* here, we are passing in an int and a function. we are
+     allowed to do this because the function we are passing 
+     in returns an int */
   printf("%d squared is %d\n", number, square_this(number));
 }
